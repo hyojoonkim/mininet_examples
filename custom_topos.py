eab5cc87acf241d70ce76f6c3558e572bdffb629
hyojoonkim <joonk@gatech.edu>
2013-08-13 09:29:41 -0700

    add general fattree generation

--- custom_topos.py
+++ custom_topos.py
@@ -4,13 +4,39 @@
 ################################################################################
 
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController   
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.util import irange,dumpNodeConnections
+from mininet.log import setLogLevel
 
-class FatTree_basic( Topo ):
+class Clique_3sw( Topo ):
   def __init__(self):
     # Initialize topology
     Topo.__init__( self )
 
-    # TODO
+    # Add hosts and switches
+    h1 = self.addHost( 'h1' )
+    h2 = self.addHost( 'h2' )
+    h3 = self.addHost( 'h3' )
+
+    s1 = self.addSwitch( 's1' )
+    s2 = self.addSwitch( 's2' )
+    s3 = self.addSwitch( 's3' )
+
+    # Add links
+    self.addLink( h1, s1 )
+    self.addLink( h2, s2 )
+    self.addLink( h3, s3 )
+
+    for i in range(3):
+      for j in range(2):
+        if (i+1)<(j+2):
+          if i==1 and j==3:
+            self.addLink ( 's%s'%(i+1), 's%s'%(j+2), delay='100ms')
+          self.addLink ( 's%s'%(i+1), 's%s'%(j+2))
 
 
 class Clique_4sw( Topo ):
@@ -40,7 +66,7 @@ class Clique_4sw( Topo ):
         if (i+1)<(j+2):
           self.addLink ( 's%s'%(i+1), 's%s'%(j+2))
 
-class FatTree_5sw( Topo ):
+class Server_LB( Topo ):
   def __init__(self):
     # Initialize topology
     Topo.__init__( self )
@@ -49,24 +75,56 @@ class FatTree_5sw( Topo ):
     h1 = self.addHost( 'h1' )
     h2 = self.addHost( 'h2' )
     h3 = self.addHost( 'h3' )
+    h4 = self.addHost( 'h4' )
 
     s1 = self.addSwitch( 's1' )
     s2 = self.addSwitch( 's2' )
     s3 = self.addSwitch( 's3' )
-    s4 = self.addSwitch( 's4' )
-    s5 = self.addSwitch( 's5' )
 
     # Add links
     self.addLink( h1, s1 )
     self.addLink( h2, s2 )
-    self.addLink( h3, s3 )
+    self.addLink( h3, s2, delay='100ms')
+    self.addLink( h4, s3 )
+    
+    self.addLink( s1, s2 )
+    self.addLink( s2, s3)
 
-    self.addLink( s4, s1 )
-    self.addLink( s4, s2 )
-    self.addLink( s4, s3 )
+class Fattree_general( Topo ):
+  def __init__(self, top, bottom, hostfanout):
+    Topo.__init__(self)
 
-    self.addLink( s5, s1 )
-    self.addLink( s5, s2 )
-    self.addLink( s5, s3 )
+    top_switches = []  
+    bottom_switches = []  
+    host_machines = []
 
-topos = { 'fattree_5sw': ( lambda: FatTree_5sw() ), 'fattree_basic': ( lambda: FatTree_basic() ), 'clique_4sw' : ( lambda: Clique_4sw() )}
+    # Create top switches
+    for s in range(top):
+      top_switches.append(self.addSwitch( 's%s'%(s+1) ))
+
+    # Create bottom switches and hosts
+    for s in range(bottom):
+      bottom_switches.append(self.addSwitch( 's%s'%(s+1+top) ))
+
+      # Host creation
+      for h in range(hostfanout):
+        host_machines.append(self.addHost( 'h%s'%(h+1+s*hostfanout) ))
+
+    # Wiring of top and bottom, bottom and hosts
+    for idx,b in enumerate(bottom_switches):
+      for t in top_switches:
+        self.addLink ( b, t )
+
+      # Wiring hosts and bottom switches
+      for h in range(hostfanout):
+        self.addLink( host_machines[h + idx*hostfanout], b )
+
+
+
+
+##### Topologies #####
+topos = { 'fattree_2_3_1': ( lambda: Fattree_general(2,3,1) ),          \
+          'clique_4sw' : ( lambda: Clique_4sw() ),           \
+          'clique_3sw' : ( lambda: Clique_3sw() ),           \
+          'server_lb' : ( lambda: Server_LB() ),             \
+          'fattree_2_5_1' : ( lambda: Fattree_general(2,5,1) ) }
